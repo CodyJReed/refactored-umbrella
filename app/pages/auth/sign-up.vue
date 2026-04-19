728f522eadac5aebfac5d617e46bfb739d13795e
CodyJReed <cody2j2reed@gmail.com>
2026-04-19 10:16:12 -0400

    add /layout auth;factor /sign-up for form functionality

--- app/pages/auth/sign-up.vue
+++ app/pages/auth/sign-up.vue
@@ -1,3 +1,66 @@
 <template>
-    <h1>Sign Up</h1>
+  <div class="flex items-center justify-center">
+    <div class="w-full max-w-md p-6">
+      <h1 class="font-anton text-center mb-6 text-5xl">
+        {{ type === "sign-up" ? "Sign up" : "Sign in" }}
+      </h1>
+
+      <UForm :state="formData" @submit="onSubmit" class="space-y-4">
+        <UFormField label="email" name="email">
+          <UInput v-model="formData.email" class="w-full" />
+        </UFormField>
+
+        <UFormField label="password" name="password">
+          <UInput v-model="formData.password" type="password" class="w-full" />
+        </UFormField>
+
+        <UButton
+          type="button"
+          trailing-icon="i-lucide-arrow-left"
+          variant="text"
+          color="grey"
+          @click="navigateTo('/')"
+          class="cursor-pointer"
+        >
+          Go back
+        </UButton>
+        <UButton type="submit" trailing-icon="i-lucide-arrow-right" :loading>
+          {{ type === "sign-up" ? "Sign up" : "Sign in" }}
+        </UButton>
+
+        <USeparator />
+
+        <UButton
+          trailing-icon="mdi:account-alert-outline"
+          variant="ghost"
+          color="info"
+          @click="type = type === 'sign-up' ? 'sign-in' : 'sign-up'"
+          class="cursor-pointer"
+        >
+          {{ type === "sign-up" ? "I wnat to sign in" : "I want to sign up" }}
+        </UButton>
+      </UForm>
+    </div>
+  </div>
 </template>
+
+<script setup>
+import * as yup from "yup";
+
+const type = ref("sign-up");
+const loading = ref(false);
+const formData = reactive({
+  email: "cody@test.com",
+  password: "abc123",
+});
+
+async function onSubmit(event) {
+  event.preventDefault();
+
+  console.log(formData.value);
+}
+
+definePageMeta({
+  layout: "auth",
+});
+</script>
