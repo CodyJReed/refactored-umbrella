f2087805de7051a800be84dfed299d28b9f4d592
CodyJReed <cody_j_reed@proton.me>
2026-04-22 10:48:03 -0400

    create associated event model && .../api/events route handling

---
+++ server/utils/models/user-event.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const UserEventSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Object,
+    default: null,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ["pending", "completed"],
+    default: "pending",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export default mongoose.models.UserEvent ||
+  mongoose.model("UserEvent", UserEventSchema);
