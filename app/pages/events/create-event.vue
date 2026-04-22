b21eb698e29107a8a91a89db942821171370df04
CodyJReed <cody_j_reed@proton.me>
2026-04-22 10:47:28 -0400

    add structure, styling, and initial functionality placeholders

--- app/pages/events/create-event.vue
+++ app/pages/events/create-event.vue
@@ -1,10 +1,66 @@
 <template>
-    <h1>Create Evenets</h1>
+  <div class="flex items-center justify-center">
+    <div class="w-full max-w-md p-6">
+      <h1 class="font-anton text-left mb-6 text-5xl">Create Event</h1>
+      <UForm
+        :schema="schema"
+        :state="formData"
+        class="space-y-4"
+        @submit="onSubmit"
+      >
+        <UFormField label="Title" name="title">
+          <UInput v-model="formData.title" class="w-full" placeholder="Title" />
+        </UFormField>
+        <UFormField label="Content" name="content">
+          <UTextarea
+            autoresize
+            v-model="formData.content"
+            class="w-full"
+            placeholder="Lorem Ipsum"
+          />
+        </UFormField>
+        <UPopover>
+          <UButton color="neutral" variant="subtle" icon="i-lucide-calendar">{{
+            formData.date
+              ? format(formData.date, "d MMM, yyyy")
+              : "Select a date"
+          }}</UButton>
+          <template #content>
+            <UCalendar class="p-2" v-model="formData.date" />
+          </template>
+        </UPopover>
+        <UButton type="submit" :loading="loading" class="w-full" size="lg"
+          >Create event</UButton
+        >
+      </UForm>
+    </div>
+  </div>
 </template>
 
 <script setup>
+import * as yup from "yup";
+import { format } from "date-fns";
+const toast = useToast();
+
+const loading = ref(false);
+const errors = ref([]);
+const formData = reactive({
+  title: "",
+  content: "",
+  date: null,
+});
+
+const schema = yup.object({
+  title: yup.string().required("Title is required."),
+  content: yup.string().required("Text is required."),
+});
+
+async function onSubmit(event) {
+  event.preventDefault();
+  loading.value = true;
+}
 
 definePageMeta({
-    middleware: ['auth-guard']
-})
+  middleware: ["auth-guard"],
+});
 </script>
